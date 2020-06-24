8a6636a68338f902617e4fec718e2f0a6e4de888
Julian Stephens <julianstephens55@gmail.com>
2020-06-24 14:59:38 -0400

    Finish queue implementation and tests

--- StacksQueues/queue.py
+++ StacksQueues/queue.py
@@ -6,13 +6,26 @@ class Queue:
        self.size = 0
        self.front = 0
 
+    def __compress(self): 
+        """Compresses backing list
+
+        Allows amortized O(1) dequeue and prevents list from growing unnecessarily
+        """
+        compresssed = []
+
+        for i in range(self.front, len(self.backingList)): 
+            compresssed.append(self.backingList[i])
+
+        self.backingList = compresssed
+        self.front = 0
+
     def isEmpty(self):
         """Checks if backing list is empty
 
         Returns:
             bool: True for empty, False otherwise
         """
-        return len(self.backingList) == 0 
+        return len(self.backingList) == self.front 
 
     def checkData(self, data): 
         """Checks if data is null
@@ -36,6 +49,19 @@ class Queue:
         """
         return self.size
 
+    def getFront(self): 
+        """Gets item at the front of the queue
+
+        O(1) for all cases
+
+        Returns: 
+            int: Data at the front of the queue
+        """
+        if self.isEmpty(): 
+            raise RuntimeError("Cannot access front of an empty queue.")
+        
+        return self.backingList[self.front]
+
     def toList(self):
         """Returns the queue's backing list
 
@@ -49,7 +75,7 @@ class Queue:
     def enqueue(self, data): 
         """Adds given data to the queue
 
-        O(1) for all cases
+        Amortized O(1)
 
         Args:
             data (int): The data to be added
@@ -60,7 +86,7 @@ class Queue:
         self.size += 1
 
     def dequeue(self): 
-        """Removes the data at the top of the queue 
+        """Removes the data the front of the queue 
 
         O(1) in all cases
 
@@ -73,8 +99,12 @@ class Queue:
         if self.isEmpty(): 
             raise RuntimeError("Cannot dequeue from an empty queue.")
         
-        removed = self.peek()
-        self.backingList.pop()
+        if self.front * 2 > len(self.backingList): 
+            self.__compress()
+
+        removed = self.backingList[self.front]
+        self.backingList[self.front] = None
+        self.front += 1
         self.size -= 1
 
         return removed
@@ -90,5 +120,5 @@ class Queue:
         if self.isEmpty(): 
             raise RuntimeError("Cannot peek an empty queue.")
 
-        return self.backingList[-1]
+        return self.backingList[self.front]
 
--- StacksQueues/tests.py
+++ StacksQueues/tests.py
@@ -2,6 +2,7 @@ import unittest
 import time
 import timeout_decorator
 from stack import Stack
+from queue import Queue
 
 class TestStack(unittest.TestCase): 
     stack = None
@@ -66,6 +67,70 @@ class TestStack(unittest.TestCase):
     
         self.assertEqual(self.stack.peek(), 59)
         self.assertEqual(self.stack.getSize(), 4)
+
+
+class TestQueue(unittest.TestCase): 
+    queue = None
+    TIMEOUT = 200
+
+    @timeout_decorator.timeout(TIMEOUT)
+    def setUp(self): 
+        self.queue = Queue()
+
+    @timeout_decorator.timeout(TIMEOUT)
+    def test_init(self): 
+        self.assertEqual(self.queue.getSize(), 0)
+
+    @timeout_decorator.timeout(TIMEOUT)
+    def test_enqueue(self): 
+        self.assertEqual(self.queue.getSize(), 0)
+        
+        self.queue.enqueue(34)
+        self.queue.enqueue(29)
+        self.queue.enqueue(48)
+        self.queue.enqueue(59)
+
+        with self.assertRaises(ValueError) as context: 
+            self.queue.enqueue(None)
+        
+        self.assertEqual(self.queue.getSize(), 4)
+        
+        expected = [34, 29, 48, 59]
+
+        self.assertEqual(self.queue.toList(), expected) 
+
+    @timeout_decorator.timeout(TIMEOUT)
+    def test_dequeue(self): 
+        self.assertEqual(self.queue.getSize(), 0)
+
+        self.queue.enqueue(34)
+        self.queue.enqueue(29)
+        self.queue.enqueue(48)
+        self.queue.enqueue(59)
+
+        self.assertEqual(self.queue.dequeue(), 34)
+        self.assertEqual(self.queue.getSize(), 3)
+        
+        expected = [None, 29, 48, 59]
+
+        self.assertEqual(self.queue.toList(), expected) 
+        
+    @timeout_decorator.timeout(TIMEOUT)
+    def test_peek(self): 
+        self.assertEqual(self.queue.getSize(), 0)
+
+        self.queue.enqueue(34)
+        self.queue.enqueue(29)
+        self.queue.enqueue(48)
+        self.queue.enqueue(59)
+
+        self.assertEqual(self.queue.peek(), 34)
+        self.assertEqual(self.queue.getSize(), 4)
+        
+        expected = [34, 29, 48, 59]
+
+        self.assertEqual(self.queue.toList(), expected) 
+
 
 
 if __name__ == "__main__": 
